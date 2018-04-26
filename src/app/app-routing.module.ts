42afcf817c6f0347953639147e93005dd142a47d
ryan0122 <ryan.mack1200@gmail.com>
2018-04-25 22:10:01 -0600

    styling updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,23 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import {ServicesComponent} from './services/services.component';
-import {ContactComponent} from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(
-      [
-        {path: 'home', component: HomeComponent},
-        {path: 'about', component: AboutComponent},
-        {path: 'services', component: ServicesComponent},
-        {path: 'portfolio', component: PortfolioComponent},
-        {path: 'contact', component: ContactComponent},
-        {path: '', redirectTo: 'home', pathMatch: 'full'},
-        {path: '**', component: PageNotFoundComponent}
-      ]
-    )
+    RouterModule.forRoot(routes)
   ],
   exports: [ RouterModule ]
 })
